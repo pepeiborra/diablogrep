24239feb42201e58390f0c8f863e27e4a7d0e37c
José Iborra <pepeiborra@gmail.com>
2014-05-07 21:04:25 +0100

    Fix for 2 handed weapons

--- types.hs
+++ types.hs
@@ -18,7 +18,7 @@ data Gem    = Gem { gem_attributesRaw :: AttributeMap } deriving (Eq,Ord)
 type AttributeMap = Map String Attribute
 data Attribute = Attr {min :: Double, max :: Double} deriving (Eq,Show,Ord)
 data HeroDetails item = HeroD { items :: ItemSet item } deriving (Eq,Show,Ord,Functor,Foldable,Traversable)
-data ItemSet item = ItemSet { neck, waist, feet, bracers, torso, hands, head, legs, leftFinger, rightFinger, shoulders, offHand :: item} deriving (Eq,Show,Ord,Functor,Foldable,Traversable)
+data ItemSet item = ItemSet { neck, waist, feet, bracers, torso, hands, head, legs, leftFinger, rightFinger, shoulders :: item, offhand :: Maybe item} deriving (Eq,Show,Ord,Functor,Foldable,Traversable)
 data ItemRef = ItemRef {tooltipParams :: String} deriving (Eq,Show,Ord)
 
 $(deriveJSON defaultOptions ''Career)
